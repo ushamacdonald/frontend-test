682da2b8facce690a40812e678f562eafca782d7
Usha <usha@Ushas-MacBook-Air.local>
2018-04-16 09:51:00 +1200

    header completed

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
 const Header = () => (
-  <div className="section">
-    Header
+  <div className="section is-small">
+    <h1 className="title is-1 has-text-centered">A front end test of sorts.</h1>
   </div>
 )
 
