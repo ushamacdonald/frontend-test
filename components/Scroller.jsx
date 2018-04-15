4058b28355c4152c3aad59e4dc3bba49ddb05544
Usha <usha@Ushas-MacBook-Air.local>
2018-04-16 10:59:18 +1200

    Scroller working

--- components/Scroller.jsx
+++ components/Scroller.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 
-const Scroller = () => (
-  <div className="section">
-    Scroller
-  </div>
-)
+import {Carousel} from 'react-responsive-carousel'
+import css from 'react-responsive-carousel/lib/styles/carousel.min.css'
+
+const Scroller = () => (<div className="section column is-8 is-offset-2">
+  <Carousel showStatus={false} showIndicators={false} showThumbs={false} centerMode centerSlidePercentage={30}>
+    <div>
+      <img src="./images/silvereye.jpg"/>
+    </div>
+    <div>
+      <img src="./images/kea.jpg"/>
+    </div>
+    <div>
+      <img src="./images/tui.jpg"/>
+    </div>
+    <div>
+      <img src="./images/penguin.jpg"/>
+    </div>
+  </Carousel>
+</div>)
 
 export default Scroller
